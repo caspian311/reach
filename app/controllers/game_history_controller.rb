b332eb503a45282063fde8c59701cf0ad9dd7acc
Matt Todd <matt.c.todd@gmail.com>
2011-06-03 06:15:43 -0500

    refactored a bit

--- app/controllers/game_history_controller.rb
+++ app/controllers/game_history_controller.rb
@@ -4,20 +4,41 @@ class GameHistoryController < ActionController::Base
    layout "application"
 
    def show
+      @page = current_page(params)
+      @title = "Game History"
+
+      @total_pages = ReachGame.all.count / 10
+
+      @pages = pages_to_display(@page, @total_pages)
+
+      @games = ReachGame.offset(@page * GAMES_PER_PAGE).limit(GAMES_PER_PAGE).find(:all, :order => "timestamp DESC")
+   end
+
+   private
+   def current_page(params)
       @page = params[:page]
       if @page == nil
          @page = 0
       else
          @page = @page.to_i
       end
-      @title = "Game History"
+   end
 
-      @total_pages = ReachGame.all.count / 10
+   def pages_to_display(current_page, total_pages)
+      all_pages = (0..total_pages).to_a
 
-      all_pages = (0..@total_pages).to_a
+      if all_pages.length > 5
+         if current_page < 3 # at the beginning, show first 5
+            display_pages = all_pages.first(5)
+         elsif current_page > (total_pages - 4) # at the end, show last 5
+            display_pages = all_pages.last(5)
+         else
+            display_pages = all_pages[(current_page - 2)..(current_page + 2)]
+         end
+      else
+         display_pages = all_pages
+      end
 
-      @pages = all_pages
-
-      @games = ReachGame.offset(@page * GAMES_PER_PAGE).limit(GAMES_PER_PAGE).find(:all, :order => "timestamp DESC")
+      display_pages
    end
 end
