8df80a3481aaa6abb0c033fb5088d716cb4499da
Matt Todd <matt.c.todd@gmail.com>
2011-06-07 07:30:19 -0500

    added the number of games played on the effectiveness page.  added some formatting on the numbers.

--- app/models/player_effectiveness_model.rb
+++ app/models/player_effectiveness_model.rb
@@ -2,7 +2,8 @@ class PlayerEffectivenessModel
    def self.stats_for_map(map_name)
       sql = "  SELECT         p.real_name as player_name,
                               m.name as map,
-                              (avg((1.0 * team_score) + 1) / ((1.0 * team_size) / (1.0 * other_team_size))) as effectiveness
+                              (avg((1.0 * team_score) + 1) / ((1.0 * team_size) / (1.0 * other_team_size))) as effectiveness,
+                              count(*) as number_of_games
                FROM           player_effectivenesses pe, players p, reach_maps m
                WHERE          m.name = '#{map_name}'
                AND            m.id = pe.reach_map_id
@@ -17,6 +18,7 @@ class PlayerEffectivenessModel
          player_effectiveness_stat.player_name = row.player_name
          player_effectiveness_stat.map = row.map
          player_effectiveness_stat.effectiveness = row.effectiveness
+         player_effectiveness_stat.number_of_games = row.number_of_games
 
          player_effectiveness_stats << player_effectiveness_stat
       end
