6dece824b2efe0c9280664623562c504db223b29
Matt Todd <matt.c.todd@gmail.com>
2011-07-08 06:20:17 -0500

    re-annotated medal model

--- app/models/medal.rb
+++ app/models/medal.rb
@@ -1,11 +1,12 @@
 # == Schema Information
-# Schema version: 2
+# Schema version: 1
 #
 # Table name: medals
 #
 #  id          :integer(4)      not null, primary key
 #  name        :string(255)
 #  description :string(255)
+#  image       :string(255)
 #
 
 class Medal < ActiveRecord::Base
