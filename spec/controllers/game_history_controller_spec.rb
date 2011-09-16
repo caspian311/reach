8cf1642bacfecea8e12c704608598361ab30d549
Matt Todd <matt.c.todd@gmail.com>
2011-09-16 17:16:43 -0500

    added some spec coverage of the game history controller change

--- spec/controllers/game_history_controller_spec.rb
+++ spec/controllers/game_history_controller_spec.rb
@@ -8,6 +8,13 @@ describe GameHistoryController do
        end
    end
 
+   describe "GET show particular game" do
+      it "should be successful" do
+         get "show", :game => 123
+         response.should be_success
+       end
+   end
+
    describe "GET info" do
       it "should be successful" do
          get "info"
@@ -22,5 +29,15 @@ describe GameHistoryController do
          get "search", :game_id => 1
          response.should redirect_to(:action => "show", :page => 4, :game => 1)
        end
+   end
+
+   describe "showing a particular game" do
+      it "should indicate the game" do
+         game_id = 123
+
+         get "show", :game => game_id
+
+         assigns(:show_game).should == game_id
+      end
    end
 end
--- spec/requests/game_history_requests_spec.rb
+++ spec/requests/game_history_requests_spec.rb
@@ -3,14 +3,10 @@ require "spec_helper"
 describe "Game History" do
    describe "listing" do
       it "should show some games" do
-         get game_history_page
+         get "/game_history"
 
          response.should render_template("game_history/show")
       end
-   end
-
-   def game_history_page
-      "/game_history"
    end
 end
 
