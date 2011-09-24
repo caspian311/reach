9bf95a916a6c8bfc58378202ed5177278cbe4de1
Matt Todd <matt.c.todd@gmail.com>
2011-09-23 20:47:38 -0500

    fixed specs for new data

--- spec/requests/player_stats_controller_requests_spec.rb
+++ spec/requests/player_stats_controller_requests_spec.rb
@@ -13,6 +13,8 @@ describe "Player Graphs" do
       @medal3_name = medals(:medal3).name
       @game1_id = reach_games(:game1).id
       @game1_timestamp = reach_games(:game1).game_time.getlocal.strftime("%m/%d/%Y %I:%M%p")
+      @game2_id = reach_games(:game2).id
+      @game2_timestamp = reach_games(:game2).game_time.getlocal.strftime("%m/%d/%Y %I:%M%p")
    end
 
    describe "fetching player kill/death stats data for player and map" do
@@ -22,24 +24,30 @@ describe "Player Graphs" do
          player_stats_data = JSON.parse(response.body)
 
          assert_equal 2, player_stats_data["stats"].size
-         assert_equal 1, player_stats_data["graph_meta_data"].size
+         assert_equal 2, player_stats_data["graph_meta_data"].size
 
-         game_id = player_stats_data["graph_meta_data"][0]["id"]
-         game_description = player_stats_data["graph_meta_data"][0]["description"]
+         game1_id = player_stats_data["graph_meta_data"][1]["id"]
+         game1_description = player_stats_data["graph_meta_data"][1]["description"]
+         game2_id = player_stats_data["graph_meta_data"][0]["id"]
+         game2_description = player_stats_data["graph_meta_data"][0]["description"]
 
-         assert_equal @game1_id, game_id
-         assert_equal "#{@game1_timestamp}<br />Capture the Flag <br />Map One", game_description
+         assert_equal @game1_id, game1_id
+         assert_equal "#{@game1_timestamp}<br />Capture the Flag <br />Map One", game1_description
+         assert_equal @game2_id, game2_id
+         assert_equal "#{@game2_timestamp}<br />Capture the Flag <br />Map One", game2_description
 
          kills_data = player_stats_data["stats"][0]
          deaths_data = player_stats_data["stats"][1]
 
          assert_equal "Kills", kills_data["label"]
-         assert_equal 1, kills_data["data"].size
-         assert_equal [0, 10], kills_data["data"][0]
+         assert_equal 2, kills_data["data"].size
+         assert_equal [0, 7], kills_data["data"][0]
+         assert_equal [1, 10], kills_data["data"][1]
 
          assert_equal "Deaths", deaths_data["label"]
-         assert_equal 1, deaths_data["data"].size
-         assert_equal [0, 5], deaths_data["data"][0]
+         assert_equal 2, deaths_data["data"].size
+         assert_equal [0, 6], deaths_data["data"][0]
+         assert_equal [1, 5], deaths_data["data"][1]
       end
    end
 
@@ -50,25 +58,29 @@ describe "Player Graphs" do
          player_stats_data = JSON.parse(response.body)
 
          assert_equal 2, player_stats_data["stats"].size
-         assert_equal 1, player_stats_data["graph_meta_data"].size
+         assert_equal 2, player_stats_data["graph_meta_data"].size
 
-         game_id = player_stats_data["graph_meta_data"][0]["id"]
-         game_description = player_stats_data["graph_meta_data"][0]["description"]
+         game1_id = player_stats_data["graph_meta_data"][0]["id"]
+         game1_description = player_stats_data["graph_meta_data"][0]["description"]
+         game2_id = player_stats_data["graph_meta_data"][1]["id"]
+         game2_description = player_stats_data["graph_meta_data"][1]["description"]
 
-         assert_equal @game1_id, game_id
-         assert_equal "#{@game1_timestamp}<br />Capture the Flag <br />Map One", game_description
+         assert_equal @game1_id, game1_id
+         assert_equal "#{@game1_timestamp}<br />Capture the Flag <br />Map One", game1_description
+         assert_equal @game2_id, game2_id
+         assert_equal "#{@game2_timestamp}<br />Capture the Flag <br />Map One", game2_description
 
          each_game_data = player_stats_data["stats"][0]
          average_data = player_stats_data["stats"][1]
 
          assert_equal "Each game", each_game_data["label"]
-         assert_equal 1, each_game_data["data"].size
+         assert_equal 2, each_game_data["data"].size
          assert_equal [0, 4.0], each_game_data["data"][0]
 
          assert_equal "Average", average_data["label"]
-         assert_equal 1, average_data["data"].size
+         assert_equal 2, average_data["data"].size
          assert_equal 0, average_data["data"][0][0]
-         assert_equal 4.0, average_data["data"][0][1]
+         assert_equal 3.0, average_data["data"][0][1]
       end
    end
 
