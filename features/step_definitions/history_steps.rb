210e7b8b6ecf5adccfca4e5a8f9f1e4936153119
Matt Todd <matt.c.todd@gmail.com>
2011-10-03 06:43:29 -0500

    refactored step def

--- features/step_definitions/history_steps.rb
+++ features/step_definitions/history_steps.rb
@@ -1,9 +1,16 @@
 
 Then /^I should see the following table data:$/ do |table|
    table.hashes.each_with_index do |data, index|
-      data.keys.each do |type|
-         wait_until { page.has_xpath?("//td[@class='#{type}']") }
-         all("td.#{type}")[index].should have_content(data[type])
+      wait_until do 
+         first_element = table.raw[0][0]
+         page.has_xpath?("//td[@class='#{first_element}']")
+      end
+
+      data.keys.each do |class_name|
+         value = data[class_name]
+         node = all("td.#{class_name}")[index]
+         
+         node.should have_content(value)
       end
    end
 end
